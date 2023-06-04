3d7ee24a398fc434dcbdc27a5d5843063145d2cb
Franzua Plasencia <franzua.15pc@gmail.com>
2023-06-04 11:11:42 -0500

    Initialized dotenv files

---
+++ lib/config/constants/environment.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+  static String apiUrl = dotenv.env['API_URL'] ?? 'No API_URL env variable';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/config/config.dart';
 import 'package:teslo_shop/config/router/app_router.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+
   runApp(
     ProviderScope(
       child: const MainApp(),
