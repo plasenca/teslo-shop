5951d64272eae8f5ae57c0c7fcf3fad3cddcb2ba
Franzua Plasencia <franzua.15pc@gmail.com>
2023-06-04 14:05:56 -0500

    Fix Repository parameters

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,6 +2,10 @@ import 'package:teslo_shop/features/auth/domain/domain.dart' show User;
 
 abstract class AuthRepository {
   Future<User> login(String email, String password);
-  Future<User> register({String email, String password, String fullname});
+  Future<User> register({
+    required String email,
+    required String password,
+    required String fullname,
+  });
   Future<User> checkAuthStatus(String token);
 }
