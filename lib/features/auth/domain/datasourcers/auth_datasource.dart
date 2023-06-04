4fa67969d94642e8bf07c806e6ac93ab2dad5b79
Franzua Plasencia <franzua.15pc@gmail.com>
2023-06-04 11:36:48 -0500

    Fix Auth Datasource parameters

--- lib/features/auth/domain/datasourcers/auth_datasource.dart
+++ lib/features/auth/domain/datasourcers/auth_datasource.dart
@@ -2,6 +2,10 @@ import 'package:teslo_shop/features/auth/domain/domain.dart' show User;
 
 abstract class AuthDatasource {
   Future<User> login(String email, String password);
-  Future<User> register({String email, String password, String fullname});
+  Future<User> register({
+    required String email,
+    required String password,
+    required String fullname,
+  });
   Future<User> checkAuthStatus(String token);
 }
