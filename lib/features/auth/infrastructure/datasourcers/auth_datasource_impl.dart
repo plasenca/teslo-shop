3b0517ac7e1fdca37e855e12acc6feabc174affb
Franzua Plasencia <franzua.15pc@gmail.com>
2023-06-04 17:17:40 -0500

    Implement login function in AuthDataSourceImpl

--- lib/features/auth/infrastructure/datasourcers/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasourcers/auth_datasource_impl.dart
@@ -1,8 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/config.dart' show Environment;
+
 import 'package:teslo_shop/features/auth/domain/domain.dart'
-    show AuthDatasource;
-import 'package:teslo_shop/features/auth/domain/entities/user.dart';
+    show AuthDatasource, User;
+import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart'
+    show UserMapper, WrongCredentials;
 
 class AuthDataSourceImpl extends AuthDatasource {
+  final dio = Dio(BaseOptions(
+    baseUrl: Environment.apiUrl,
+  ));
+
   @override
   Future<User> checkAuthStatus(String token) {
     // TODO: implement checkAuthStatus
@@ -10,9 +18,19 @@ class AuthDataSourceImpl extends AuthDatasource {
   }
 
   @override
-  Future<User> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+    try {
+      final response = await dio.post('/auth/login', data: {
+        'email': email,
+        'password': password,
+      });
+
+      final user = UserMapper.userJsonToEntity(response.data);
+
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
   }
 
   @override
